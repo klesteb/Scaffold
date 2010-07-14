7d526e9c09f4b37f42fd46827301623fc4288705
kesteb <kesteb@c9dd8d53-b656-4de8-a331-d8a3666674b0>
2010-07-14 16:16:45 +0000

    Updated documentation, making the package ready for release.

--- trunk/lib/Scaffold.pm
+++ trunk/lib/Scaffold.pm
@@ -49,12 +49,13 @@ session management, resource locking, templating and a URL dispatching system,
 without any preconceived notions on how I should write my code. Scaffold is 
 my attempt to do this. I hope it works for you too.
 
-=head1 THE CLASS SYSTEM
+=head1 THE BASIC SYSTEM
 
-Object Oriented Perl. There are a lot of ways of doing it. Some are considered
-the "next best thing" and/or a "best practice" for Perl and everybody should 
-use it, regardless of usability or fitness for that purpose. I choose to use 
-the Badger toolkit. Why, because it made sense, it's as simple as that. 
+Scaffold is an OO based system and OO Perl can be done many 
+different ways. Some are considered the "next best thing" and/or a "best 
+practice" for Perl and everybody should use it, regardless of usability or 
+fitness for that purpose. I choose to use the Badger toolkit. Why, because 
+it made sense, it's as simple as that.
 
 Since the class system is built upon Badger, it follows some of Badger's
 methodology. The base class for Scaffold is Scaffold::Base. This loads 
@@ -62,7 +63,9 @@ Badger::Base, with all of it's goodness, defines some error messages and
 exposes the config() method. This method is important for other classes 
 within Scaffold. 
 
-=item Scaffold::Class 
+=over 4
+
+=item Scaffold::Class
 
 This class extends Badger::Class and loads Scaffold::Constants and 
 Scaffold::Utils. Which extends their respected Badger counter parts. 
@@ -99,17 +102,20 @@ The following states are defined:
  
 The following handlers are provided:
 
- * Scaffold::Handler::Static    - handles static pages with the option of
-                                  storing those pages in cache
+ * Scaffold::Handler::Static    - handles static pages with the 
+                                  option of storing those pages in 
+                                  cache
  * Scaffold::Handler::Favicon   - load and cache a "favicon.ico" file
  * Scaffold::Handler::Robots    - load and cache a "robots.txt" file
- * Scaffold::Handler::ExtDirect - implement the Ext.direct rpc protocol
- * Scaffold::Handler::ExtPoll   - implement the EXt.direct polling protocol 
+ * Scaffold::Handler::ExtDirect - an experimental implementation of the 
+                                  Ext.direct RPC protocol
+ * Scaffold::Handler::ExtPoll   - an experimental implementing of the 
+                                  Ext.direct polling protocol
 
 =item Scaffold::Plugin
 
-This is the base class for plugins. Plugins are global and they run in 
-loaded order. Plugins can be invoked in the following phases of a handler:
+This is the base class for plug-ins. Plug-ins are global and they run in 
+loaded order. Plug-ins can be invoked in the following phases of a handler:
 
  STATE_PRE_ACTION
  STATE_POST_ACTION
@@ -130,7 +136,7 @@ This is the base class for stashes. A stash is global to a handler.
 The following stashes have been defined:
 
  * Scaffold::Stash::View       - used to hold the output buffer
- * Scaffold::Stash::Cookie     - not currently being used
+ * Scaffold::Stash::Cookie     - used to hold the cookies
  * Scaffold::Stash::Controller - not currently being used
 
 =item Scaffold::Cache
@@ -142,7 +148,7 @@ have been defined:
  * Scaffold::Cache::Memcached
 
 Scaffold::Cache::FastMmap is loaded by default. Scaffold::Cache::Manager is
-used to manage the cache. It is implemented as a plugin and is the first one
+used to manage the cache. It is implemented as a plug-in and is the first one
 to run. It executes in the STATE_PRE_ACTION phase of a handler. It purges the
 cache of expired items.
  
@@ -151,7 +157,7 @@ cache of expired items.
 Sessions are handled by HTTP::Session. Sessions are stored in temporary cookies.
 Scaffold::Session::Store::Cache ties session storage to the internal caching 
 subsystem. Scaffold::Session::Manager initialized or reestablishes existing 
-sessions. It is implemented as a plugin. It is the second one to run and 
+sessions. It is implemented as a plug-in. It is the second one to run and 
 executes in the STATE_PRE_ACTION to establish sessions and in STATE_PRE_EXIT 
 to create or update the session cookie.
 
@@ -163,7 +169,7 @@ It is loaded by default.
 
 =item Scaffold::Uaf
 
-This implements a authorization and authentication framework. The following
+This implements an authorization and authentication framework. The following
 modules are base classes and most of them should be overridden by your code.
 
  * Scaffold::Uaf::Authenticate - this is a mixin used for authentication
@@ -183,17 +189,18 @@ The following are handlers that may be overridden:
 
 They have a dependency on Scaffold::Uaf::Authenticate.
 
-The following is a plugin that may be overridden:
+The following is a plug-in that may be overridden:
 
  * Scaffold::Uaf::Manager - a plugin that checks authentication and 
                             initiates the login process
 
 This has a dependency on Scaffold::Uaf::Authenticate 
 
+=back
 
+=head1 A SIMPLE APPLICATION
 
-For example, a simple 
-Scaffold application could be written as follows:
+For example, a simple Scaffold application could be written as follows:
 
  app.psgi
  --------
@@ -243,120 +250,63 @@ Scaffold application could be written as follows:
 
 Which would start your application on port 5000 of localhost using the 
 singled threaded Plack server engine. You can direct your browser 
-to that url and receive a resounding "Hello World!". 
+to that URL and receive a resounding "Hello World!". 
 
 See, simple, no muss, no fuss. 
 
-At its heart, Scaffold::Server is a simple url dispatcher. Sure it
-does other things, like, settting up the caching subsystem using Cache::FastMmap,
-establishing a session using HTTP::Session, while storing the session meta 
-data in the cache subsystem, establishing a resource lock manager 
-using Keyedmutex and by default it uses raw html for output. All of these 
-defaults can be overridden using the configs. But back to the class system.
+=head1 SEE ALSO
 
-Since Scaffold::Server is a simple url dispatcher, it needs to know how to 
-map urls to application modules. This is done with the B<locations> config 
-stanza. In the above example, "/" is mapped to App::Main. App::Main inherits 
-and extends Scaffold::Handler. Scaffold::Handler is a basic state machine. 
-Certain states invoke certain actions during the lifetime of a request. Some 
-of those states involve plugins. Plugins inherit and extend Scaffold::Plugin.
-What plugins are loaded is controled by the B<plugins> stanza. PLugins are 
-run in loaded order. By default two plugins are always loaded. They are 
-Scaffold::Cache::Manager which manages the cache subsystem and 
-Scaffold::Session::Manager which manages the sessions. The request process 
-is broken out into these states, which are defined in Scaffold::Constants:
+ Scaffold::Base
+ Scaffold::Class
+ Scaffold::Constants
+ Scaffold::Engine
+ Scaffold::Server
+ Scaffold::Utils
 
-=item STATE_PRE_ACTION
+ Scaffold::Cache
+ Scaffold::Cache::FastMmap
+ Scaffold::Cache::Manager
+ Scaffold::Cache::Memcached
 
-This state is used by plugins to run "pre action" stuff. For example 
-Scaffold::Cache::Manager maintains the cache subsystem during this phase, 
-it flushes expired items from the cache. While Scaffold::Session::Manager 
-checks for and establishes sessions. 
+ Scaffold::Handler
+ Scaffold::Handler::ExtDirect
+ Scaffold::Handler::ExtPoll
+ Scaffold::Handler::Favicon
+ Scaffold::Handler::Robots
+ Scaffold::Handler::Static
 
-=item STATE_ACTION
+ Scaffold::Lockmgr
+ Scaffold::Lockmgr::KeyedMutex
 
-Scaffold::Server takes the url and matches it to a handler. That handler then
-takes the url and further breaks it down to determine which methods to invoke.
-In the above example, "/" resolves to do_main(). If do_main() was not defined,
-the handler would then check for do_default(). If neither of them exists a 
-declined() exception would have been thrown. 
+ Scaffold::Plugins
 
-Now, if a url of "/test" had been passed, the following would happen. First 
-the handler would check to see if there was a do_test() method. If there was, 
-control would be passed to that method. If there wasn't, then it would check 
-to see if there is a do_main() method. If there is, control would be passed 
-to do_main() with "test" as the second parameter. If do_main() didn't exist, 
-do_default() would be checked for. If it existed, then control would be 
-passed to it with "test" as the second parameter. If none of the above, then
-a declined() excpetion would be thrown.
+ Scaffold::Render
+ Scaffold::Render::Default
+ Scaffold::Render::TT
 
-Let's try a url of "/test/1". If a do_test() method exited, "1" would be the
-second parameter. If do_test() didn't exixt and do_main() did, "test" would 
-be the second parameter and "1" would be the third parameter and likewise if
-do_main() didn't exist and do_default() did. If none of them exist, a declined()
-exception is thrown.
+ Scaffold::Session::Manager
+ Scaffold::Session::Store
+ Scaffold::Session::Store::Cache
 
+ Scaffold::Stash
+ Scaffold::Stash::Controller
+ Scaffold::Stash::Cookies
+ Scaffold::Stash::Manager
+ Scaffold::Stash::View
 
-The handler first checks to see if there is a do_main() method, then it 
-checks to see if there is a specific
-
-
-This is where the handler does it thing. For the most part the url is parsed
-into its component parts and matched to the locations that are defined. When one
-is found it is dispatched too. In the above example do_main() will handle any 
-request for "/". You could also have a do_default(). The differance is that 
-do_default() could also handle this url "/test". The "test" part of the url
-would be the second parameter to do_default(). Another way to handle that url
-would be to have a do_test() method. It all depends on how you write your
-handler.
-
-
- STATE_POST_ACTION
- STATE_PRE_RENDER
- STATE_RENDER
- STATE_POST_RENDER
- STATE_PRE_EXIT
- 
-=back
-
-
-
-
-provides certain actions 
-that can be performed during the lifetime of a request. It can action It is basically a 
-state machine. 
-
-
-So what does this application give you. Well, your application is using 
-Cache::FastMmap for caching, a session has been initiated and stored within 
-the caching subsystem, a connection to the keyedmutexd daemon has been 
-established and you are using raw html for output. 
- 
-
-
-, with ties to the internal caching subsystem.
-A distributed, resource locking manager based on Keyedmutex. An user 
-authentication and authorization framework that is easily extended
-or ignored as you see fit. The freedom to use whatever ORM you desire. I like 
-DBIx::Class, you may not. Why should I dictate this choice. The freedom to
-use whatever templating engine you want. I like Template Toolkit, you may not,
-but I have included the bindings for it. You can easily create your own. 
-When you do, please contribute it back, or you can use straight HTML. I don't 
-care. How about web server independence, thanks to Plack and the emerging 
-PSGI initinitive, that was easy to do.
-
-
-
+ Scaffold::Uaf::Authenticate
+ Scaffold::Uaf::AuthorizeFactory
+ Scaffold::Uaf::Authorize
+ Scaffold::Uaf::GrantAllRule
+ Scaffold::Uaf::Login
+ Scaffold::Uaf::Logout
+ Scaffold::Uaf::Manager
+ Scaffold::Uaf::Rule
+ Scaffold::Uaf::User
 
 =head1 AUTHOR
 
 Kevin L. Esteb, C<< <kesteb(at)wsipc.org> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-scaffold at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Scaffold>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
 
 =head1 SUPPORT
 
@@ -364,34 +314,9 @@ You can find documentation for this module with the perldoc command.
 
     perldoc Scaffold
 
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Scaffold>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Scaffold>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Scaffold>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Scaffold/>
-
-=back
-
-=head1 ACKNOWLEDGEMENTS
-
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2009 Kevin L. Esteb, all rights reserved.
+Copyright 2010 Kevin L. Esteb, all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
