52dc59a8d5043a4316d9f0f5c3454315329d2ef1
Kevin L. Esteb <kevin@kesteb.us>
2012-04-24 20:30:11 +0000

    Added documentation to Lockmgr/SharedMem.pm

--- lib/Scaffold/Lockmgr/SharedMem.pm
+++ lib/Scaffold/Lockmgr/SharedMem.pm
@@ -36,3 +36,52 @@ sub set {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+Scaffold::Lockmgr::SharedMem - Use SysV IPC for resource locking.
+
+=head1 DESCRIPTION
+
+This module is an extended version of IPC::SharedMem. It adds a set() method.
+
+=head1 METHODS
+
+=over 4
+
+=item set( STAT )
+
+=item set( NAME => VALUE [, NAME => VALUE ...] )
+
+set will set the following values of the stat structure associated with 
+the shared memory set.
+
+ uid
+ gid
+ mode (only the permission bits)
+
+set accepts either a stat object, as returned by the stat method, or a 
+list of name-value pairs.
+
+=back
+
+=head1 SEE ALSO
+
+ IPC::Semaphore
+ IPC::SharedMem
+
+=head1 AUTHOR
+
+Kevin L. Esteb, E<lt>kevin@kesteb.usE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2012 by Kevin L. Esteb
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.5 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
